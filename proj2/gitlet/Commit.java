7b4c44ab8025ecc8edf1100eea1c80cef0f57e49
Esther Lu <estrellalxy@gmail.com>
2025-05-30 23:08:28 +0200

    first day

--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -3,6 +3,7 @@ package gitlet;
 // TODO: any imports you need here
 
 import java.util.Date; // TODO: You'll likely use this in this class
+import java.util.TreeMap;
 
 /** Represents a gitlet commit object.
  *  TODO: It's a good idea to give a description here of what else this Class
@@ -21,6 +22,22 @@ public class Commit {
 
     /** The message of this Commit. */
     private String message;
+    private long CommitTime;
+    private transient Commit parent1;
+    private String parent1SHA;
+    private transient Commit parent2;
+    private String parent2SHA;
+    private TreeMap<String, String> containedblobs;
 
     /* TODO: fill in the rest of this class. */
+    public Commit(String m, long stamp) {
+        message = m;
+        CommitTime = stamp;
+    }
+
+    public Commit(String m, long stamp, Commit p1) {
+        message = m;
+        CommitTime = stamp;
+        parent1 = p1;
+    }
 }
--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -1,6 +1,9 @@
 package gitlet;
 
 import java.io.File;
+import java.util.Date;
+import java.util.TreeMap;
+
 import static gitlet.Utils.*;
 
 // TODO: any imports you need here
@@ -19,6 +22,8 @@ public class Repository {
      * comment above them describing what that variable represents and how that
      * variable is used. We've provided two examples for you.
      */
+    TreeMap<String, Commit> branches;
+    String CB;
 
     /** The current working directory. */
     public static final File CWD = new File(System.getProperty("user.dir"));
@@ -26,4 +31,24 @@ public class Repository {
     public static final File GITLET_DIR = join(CWD, ".gitlet");
 
     /* TODO: fill in the rest of this class. */
+    // Constructor: if there is already .gitlet file in CWD, abort
+    // Otherwise: create .gitlet file, set default branch, create initial commit
+    public Repository() {
+        if (GITLET_DIR.exists()) {
+            throw new GitletException("A Gitlet version-control system already exists in the current directory.");
+        }
+        GITLET_DIR.mkdir();
+        File blobs = join(".gitlet", "blob");
+        blobs.mkdir();
+        File commits = join(".gitlet", "commits");
+        commits.mkdir();
+        File staging = join(".gitlet", "staging");
+
+        Date initialTime = new Date(0);
+        long timestamp = initialTime.getTime();
+        Commit initial = new Commit("initial commit", timestamp);
+        String defaultBranch = "master";
+        branches.put(defaultBranch, initial);
+        CB = defaultBranch;
+    }
 }
